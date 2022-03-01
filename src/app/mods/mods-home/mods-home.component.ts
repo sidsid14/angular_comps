8073e429c6fb400785ea62cd8fbebb65ac063754
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-03-01 09:24:04 +0530

    Accordion Component

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen: boolean = false;
+  items = [
+    {
+      title: 'Why is the sky blue?',
+      content: 'The sky is blue because it is.',
+    },
+    {
+      title: 'What does an orange tast like?',
+      content: 'An orange taste likes an orange.',
+    },
+    {
+      title: 'What color is that cat?',
+      content: 'The cat is an orange color.',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
